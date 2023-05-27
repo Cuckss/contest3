c233f39881b7dca550a9c7e8d470b1a785ae3c2b
Cuckss <akshatchaturvedi60@gmail.com>
2023-05-27 23:38:40 +0530

    contest-3

--- script.js
+++ script.js
@@ -1,71 +1,106 @@
-function fetchDataWithThen() {
-    fetch(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-    )
-      .then(function (response) {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        renderTable(data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }
-  
-  // Fetch data using async/await syntax
-  async function fetchDataWithAsyncAwait() {
-    try {
-      const response = await fetch(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-      );
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-  
-      const data = await response.json();
-      renderTable(data);
-    } catch (error) {
-      console.error(error);
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+.then(response => response.json())
+.then(data => {
+  renderTable(data);
+})
+.catch(error => console.log(error));
+
+// Fetch data using async/await
+async function fetchData() {
+try {
+  const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+  const data = await response.json();
+  renderTable(data);
+} catch (error) {
+  console.log(error);
+}
+}
+
+// Render the data in the table
+function renderTable(data) {
+const table = document.getElementById('coinTable');
+table.innerHTML = `
+  <tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Symbol</th>
+    <th>Price</th>
+    <th>Percentage</th>
+    <th>Volume</th>
+  </tr>
+`;
+
+data.forEach(coin => {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td><img src="${coin.image}" alt="${coin.name}" width="40"></td>  
+    <td>${coin.name}</td>
+    <td>${coin.symbol}</td>
+    <td>${coin.current_price}</td>
+    <td>${coin.atl_change_percentage}</td>
+    <td>${coin.total_volume}</td>
+  `;
+  table.appendChild(row);
+});
+}
+
+// Filter the table based on search input
+function filterTable() {
+const input = document.getElementById('searchInput');
+const filter = input.value.toUpperCase();
+const table = document.getElementById('coinTable');
+const rows = table.getElementsByTagName('tr');
+
+for (let i = 1; i < rows.length; i++) {
+  const name = rows[i].getElementsByTagName('td')[0];
+  if (name) {
+    const textValue = name.textContent || name.innerText;
+    if (textValue.toUpperCase().indexOf(filter) > -1) {
+      rows[i].style.display = '';
+    } else {
+      rows[i].style.display = 'none';
     }
   }
-  
-  // Render the data in a table
-  function renderTable(data) {
-    const tableBody = document.querySelector('#cryptoTable tbody');
-    tableBody.innerHTML = '';
-  
-    data.forEach(function (crypto) {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${crypto.name}</td>
-        <td>${crypto.symbol}</td>
-        <td>${crypto.current_price}</td>
-        <td>${crypto.total_volume}</td>
-      `;
-      tableBody.appendChild(row);
-    });
-  }
-  
-  // Search functionality
-  const searchInput = document.querySelector('#searchInput');
-  searchInput.addEventListener('input', function () {
-    const searchValue = searchInput.value.toLowerCase();
-    const rows = document.querySelectorAll('#cryptoTable tbody tr');
-  
-    rows.forEach(function (row) {
-      const name = row.cells[0].textContent.toLowerCase();
-      const symbol = row.cells[1].textContent.toLowerCase();
-  
-      if (name.includes(searchValue) || symbol.includes(searchValue)) {
-        row.style.display = '';
-      } else {
-        row.style.display = 'none';
-      }
-    });
-  });
-  
+}
+}
+
+// Sort the table by market cap
+function sortTableByMarketCap() {
+const table = document.getElementById('coinTable');
+const rows = Array.from(table.getElementsByTagName('tr'));
+
+rows.shift(); // Remove table header row
+
+rows.sort((a, b) => {
+  const aValue = parseFloat(a.getElementsByTagName('td')[5].innerText);
+  const bValue = parseFloat(b.getElementsByTagName('td')[5].innerText);
+  return bValue - aValue;
+});
+
+table.innerHTML = '';
+table.appendChild(rows[0]); // Append table header row
+
+rows.forEach(row => {
+  table.appendChild(row);
+});
+}
+
+// Sort the table by percentage change
+function sortTableByPercentageChange() {
+const table = document.getElementById('coinTable');
+const rows = Array.from(table.getElementsByTagName('tr'));
+
+rows.shift(); // Remove table header row
+
+rows.sort((a, b) => {
+  const aValue = parseFloat(a.getElementsByTagName('td')[4].innerText.replace('%', ''));
+  const bValue = parseFloat(b.getElementsByTagName('td')[4].innerText.replace('%', ''));
+  return bValue - aValue;
+});
+
+table.innerHTML = '';
+table.appendChild(rows[0]); // Append table header row
+
+rows.forEach(row => {
+  table.appendChild(row);
+});
